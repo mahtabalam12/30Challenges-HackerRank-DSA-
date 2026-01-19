72aec067798c551c260c061651d9cf2f9a1471c1
Md Mahtab Alam <alammahtabph@gmail.com>
2026-01-19 16:11:03 +0530

    Update Day1.java

--- HackerRank30DaysChallenge/Day1.java
+++ HackerRank30DaysChallenge/Day1.java
@@ -1,3 +1,20 @@
+/*
+Problem Statement – Day 0: Hello, World.
+
+To complete this challenge, you must save a line of input from standard input (stdin) into a variable, print Hello, World. on the first line, and then print the value of the variable on the second line.
+
+You’ve got this!
+
+Note:
+The instructions are Java-based, but solutions can be submitted in many popular programming languages. You can switch languages using the drop-down menu above your editor. The variable name and style may differ depending on the best-practice conventions of the chosen language.
+
+Input Format
+A single line of text denoting the input string whose contents must be printed.
+
+Output Format
+Print Hello, World. on the first line.
+Print the contents of the input string on the second line.
+    */
 import java.util.*;
 
 public class Day1 {
@@ -23,3 +40,4 @@ public class Day1 {
 //Hi 
 
    
+
